3662af0582d5b714d66f1fe885e17c5e26c85480
Felipe Araujo <fetrova@gmail.com>
2017-11-05 23:51:30 -0200

    Dagger dependencies

--- app/src/main/java/com/felipearaujo/newsapp/sources/SourcesActivity.kt
+++ app/src/main/java/com/felipearaujo/newsapp/sources/SourcesActivity.kt
@@ -2,9 +2,7 @@ package com.felipearaujo.newsapp.sources
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.util.Log
 import com.felipearaujo.newsapp.R
-import com.squareup.picasso.Picasso
 import dagger.android.AndroidInjection
 import javax.inject.Inject
 
@@ -12,16 +10,11 @@ class SourcesActivity : AppCompatActivity() {
 
     @Inject
     lateinit var viewModel: SourcesViewModel
-    @Inject
-    lateinit var picasso: Picasso
 
     override fun onCreate(savedInstanceState: Bundle?) {
         AndroidInjection.inject(this)
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        viewModel.print()
-        Log.v("", "Teste $picasso.isLoggingEnabled")
     }
 }
--- data/src/main/java/com/felipearaujo/data/remote/RemoteNewsRepository.kt
+++ data/src/main/java/com/felipearaujo/data/remote/RemoteNewsRepository.kt
@@ -1,7 +1,6 @@
 package com.felipearaujo.data.remote
 
 import retrofit2.Retrofit
-import javax.inject.Inject
 
 /**
  * Created by felipearaujo on 05/11/17.
