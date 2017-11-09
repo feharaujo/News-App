4dbae84b493c888385a53b1200338a1c861e1fef
Felipe Trova <fetrova@gmail.com>
2017-11-09 17:25:41 -0200

    Toolbar back button

--- app/src/main/java/com/felipearaujo/newsapp/articles/ArticlesActivity.kt
+++ app/src/main/java/com/felipearaujo/newsapp/articles/ArticlesActivity.kt
@@ -2,6 +2,9 @@ package com.felipearaujo.newsapp.articles
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.view.MenuItem
+import com.felipearaujo.data.INTENT_SOURCE
+import com.felipearaujo.model.Source
 import com.felipearaujo.newsapp.BaseActivity
 import com.felipearaujo.newsapp.R
 import dagger.android.AndroidInjector
@@ -14,10 +17,26 @@ class ArticlesActivity : BaseActivity(), HasSupportFragmentInjector {
     @Inject
     lateinit var fragmentDispatchingAndroidInjector: DispatchingAndroidInjector<Fragment>
 
-    // TODO: android injection to base
+    private val mSource: Source by lazy {
+        intent.getParcelableExtra<Source>(INTENT_SOURCE)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_articles)
+
+        title = mSource.name
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        if (item?.itemId == android.R.id.home) {
+            finish()
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     override fun supportFragmentInjector(): AndroidInjector<Fragment> {
