9749a619be7b0f6d6156ec7193054e7630a687a1
Felipe Trova de Araujo <fetrova@gmail.com>
2019-08-17 19:27:24 +0200

    test

--- sdkmanager-download-emulator.sh
+++ sdkmanager-download-emulator.sh
@@ -2,7 +2,7 @@
 
 /usr/bin/expect -c '
 set timeout -1;
-spawn '"${ANDROID_HOME}"'/tools/bin/sdkmanager "system-images;android-25;google_apis;x86";
+spawn '"${ANDROID_HOME}"'/tools/bin/sdkmanager "sys-img-x86-google_apis-24";
   expect {
     "y/N" { exp_send "y\r" ; exp_continue }
     eof
